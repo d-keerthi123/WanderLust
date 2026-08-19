cc3874e535fa4a0ac57ccf7ff3e7572bf2c336d3
Danda Keerthi <dandakeerthi2@gmail.com>
2026-08-19 21:06:43 +0530

    CRUD Operations

--- app.js
+++ app.js
@@ -40,10 +40,52 @@ app.get("/",(req,res)=>{
 // })
 
 //index route
-app.get("/listing", async (req, res) => {
+app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
     res.render("./listings/index.ejs", { allListings });
+
 });
+//new route
+app.get("/listings/new",(req,res)=>{
+    res.render("./listings/new.ejs");
+})
+
+//show route
+app.get("/listings/:id",async (req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("./listings/show.ejs",{listing})
+})
+//create route
+app.post("/listings",async (req,res)=>{
+    // let {title,description,image,price,location,country}=req.body;
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+
+})
+
+//edit route
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("./listings/edit.ejs",{listing})
+})
+
+//update route
+app.put("/listings/:id",async (req,res)=>{
+     let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+//delete route
+app.delete("/listings/:id",async (req,res)=>{
+     let {id}=req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
+
 
 app.listen(8080,()=>{
     console.log("Server is listening to port 8080");
