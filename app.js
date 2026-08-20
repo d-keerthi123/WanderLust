b108763810c483197540538c4a54900d8ac2bc13
Danda Keerthi <dandakeerthi2@gmail.com>
2026-08-20 22:41:05 +0530

    WanderLust

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate=require("ejs-mate");
 
 
 async function main(){
@@ -19,6 +20,9 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+
+app.use(express.static(path.join(__dirname,"/public")));
 
 //root
 app.get("/",(req,res)=>{
